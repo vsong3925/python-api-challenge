02a91a53a164f11887f90a294b16aa64c3c96a0e
Vera Song <verasong@Veras-MacBook-Air.local>
2020-11-19 14:21:59 -0500

    Your commit message here

--- Part-1-WeatherPy/api_keys.py
+++
@@ -1,5 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "68b2a834481c546124ccee222fa7b6a3"
-
-# Google API Key
-g_key = "YOUR KEY HERE!"
--- Part-2-VacationPy/api_keys.py
+++
@@ -1,5 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "e3be8467e8031e728ba25b25d77c65f5"
-
-# Google API Key
-g_key = "AIzaSyApq1L3STsPOnj-KVhSIX3uPHF9_E9r3BQ"
